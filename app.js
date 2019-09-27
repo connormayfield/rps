1e6e41ee18c314d0d252920c06475ded6f65e69e
connormayfield <connormayfield@gmail.com>
2019-09-27 14:38:09 -0500

    done

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ const game = () => {
     const options = document.querySelectorAll(".options button");
     const playerHand = document.querySelector(".player-hand");
     const computerHand = document.querySelector(".computer-hand");
+    const hands = document.querySelectorAll(".hands img");
+
+    hands.forEach(hand => {
+      hand.addEventListener("animationend", function() {
+        this.style.animation = "";
+      });
+    });
     //Computer Options
     const computerOptions = ["rock", "paper", "scissors"];
 
@@ -27,10 +34,12 @@ const game = () => {
         const computerNumber = Math.floor(Math.random() * 3);
         const computerChoice = computerOptions[computerNumber];
         // Here is where we call compare hands
-        compareHands(this.textContent, computerChoice);
-        //Update Images
-        playerHand.src = `./assets/${this.textContent}.png`;
-        computerHand.src = `./assets/${computerChoice}.png`;
+        setTimeout(() => {
+          compareHands(this.textContent, computerChoice);
+          //Update Images
+          playerHand.src = `./assets/${this.textContent}.png`;
+          computerHand.src = `./assets/${computerChoice}.png`;
+        }, 2000);
 
         playerHand.style.animation = "shakePlayer 2s ease";
         computerHand.style.animation = "shakeComputer 2s ease";
